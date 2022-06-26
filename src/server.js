fb315cd0a8b3b6834f9cde2ea140c66d7555467c
mnseong <mskweon@icloud.com>
2022-06-26 16:21:54 +0900

    connect mongodb

--- src/server.js
+++ src/server.js
@@ -4,6 +4,20 @@ const express = require("express")
 
 const app = express()
 
+// eslint-disable-next-line
+const { MongoClient } = require("mongodb")
+
+MongoClient.connect(
+  "mongodb+srv://minseong:qwer1234@cluster0.21z5wjm.mongodb.net/?retryWrites=true&w=majority",
+  // eslint-disable-next-line
+  (error) => {
+    if (error) return console.log("ERROR ERROR ERROR ERROR")
+    app.listen(8080, () => {
+      console.log("listening on 8080")
+    })
+  }
+)
+
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/camp", (req, res) => {
